a1b9495f93f021d20b2ec7525e20e4696c43cffb
Ubuntu <ubuntu@ip-10-255-21-175.us-west-2.compute.internal>
2013-07-10 00:24:17 +0000

    content for web.js loaded from index.html-- errors corrected

--- web.js
+++ web.js
@@ -4,10 +4,11 @@ var app = express.createServer(express.logger());
 
 var fs = require('fs');
 
-fdin_buf = fs.readFileSync("index.html")
+fdin_buf = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
   response.send(fdin_buf.toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
